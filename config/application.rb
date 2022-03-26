7bf23dae98a9ecde1eb0629c361f1ace27a59dc3
kiruke <kiruke2525@gmail.com>
2022-03-26 19:31:24 +0900

    header_footer_top

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,12 @@ module RunteqNormal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|  # ここから追記_20220323
+      g.skip_routes true      # trueの時routes.rbを変更しない
+      g.helper false          # helperファイル生成しない
+      g.test_framework false  # testファイル生成しない
+    end                       # ここまで
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
