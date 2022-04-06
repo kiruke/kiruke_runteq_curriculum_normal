b433699606f3be7f8f3911b92320863be96f15fe
kiruke <kiruke2525@gmail.com>
2022-04-06 16:46:20 +0900

    解答例前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  #デフォルトではnoticeとalertのみのため追加する
+  add_flash_types :success, :info, :warning, :danger
 end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,14 +8,17 @@ class UserSessionsController < ApplicationController
     if @user
       #もともと書いてたredirect_to動きが理解出来ていなかった
       #redirect_to root_url
+      flash[:success] = 'ログインに成功しました。'
       redirect_back_or_to root_url
     else
+      flash.now[:danger] = 'ログインに失敗しました。'
       render action: 'new'
     end
   end
 
   def destroy
     reset_session
+    flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
 end
