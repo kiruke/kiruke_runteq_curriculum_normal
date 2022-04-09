e4cdaa444cf633d0a9dbf2ca66a1b63e5704e0f3
kiruke <kiruke2525@gmail.com>
2022-04-09 21:35:32 +0900

    ログインしていない場合は掲示板機能を使わせない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   #デフォルトではnoticeとalertのみのため追加する
   add_flash_types :success, :info, :warning, :danger
+  #ログインしていなければ、掲示板機能を利用できなくする
+  before_action :login_required
+
+  private
+    def login_required
+      redirect_to login_url unless current_user
+    end
 end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,6 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :login_required
+
   def new
   end
 
