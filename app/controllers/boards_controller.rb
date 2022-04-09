986de787df72596fb35a7daafb84a3f0afe5ac08
kiruke <kiruke2525@gmail.com>
2022-04-09 17:47:16 +0900

    掲示板をパーシャルで表示

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,12 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
   end
+
+
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
+  end
 end
