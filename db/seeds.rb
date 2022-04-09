ab11302bb78fa4ccd99e80ec6bc0c7198cfaaf27
kiruke <kiruke2525@gmail.com>
2022-04-09 22:57:24 +0900

    掲示板の日時フォーマット追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+10.times do
+  User.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: 'hogehoge',
+      password_confirmation: 'hogehoge'
+  )
+end
+
 20.times do |index|
   Board.create(
     user: User.offset(rand(User.count)).first,
