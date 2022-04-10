8990d1857f45a4b9f419556df9d2104a9480e4fd
kiruke <kiruke2525@gmail.com>
2022-04-10 10:34:11 +0900

    解答後にログインしていなければ掲示板みるとフラッシュメッセージが飛ぶように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,13 @@ class ApplicationController < ActionController::Base
   before_action :login_required
 
   private
-    def login_required
-      redirect_to login_url ,warning: t('defaults.require_login') unless current_user
+    #def before_action :login_required機能を使ってログインしているか判別しているのだからunless current_user
+      #redirect_to login_url ,warning: t('defaults.require_login') unless current_user
       #2行目に書くと、unlessが読み込まれない
       #flash[:warning] = t('defaults.require_login')
+    #end
+    def not_authenticated
+      flash[:warning] = t('defaults.require_login')
+      redirect_to login_path
     end
 end
