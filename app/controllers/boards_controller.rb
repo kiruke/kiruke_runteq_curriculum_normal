8d914b4714a173341187726d430e3bb2c9e883e5
kiruke <kiruke2525@gmail.com>
2022-04-12 00:35:17 +0900

    DBリセット前

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,7 +8,14 @@ class BoardsController < ApplicationController
   end
 
   def create
-    @board = Board.new(board_params)
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      flash[:success] = t('.success')
+      redirect_back_or_to boards_url
+    else
+      flash.now[:danger] = t('.fail')
+      render action: 'new'
+    end
   end
 
   private
