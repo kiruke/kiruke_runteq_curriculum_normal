579ba792045ca1f8f5e34c1a1d02c4d0c9c4533c
kiruke <kiruke2525@gmail.com>
2022-04-28 00:19:30 +0900

    コメントコントローラのアクション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_back(fallback_location: boards_path)
+    else
+      redirect_back(fallback_location: boards_path)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+  end
 end
