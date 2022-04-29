ed04a3fa80592df731a1116c6c9ee3a099810d8a
kiruke <kiruke2525@gmail.com>
2022-04-29 13:52:43 +0900

    commentコントローラ修正 redirect_to

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,16 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = current_user.comments.build(comment_params)
-    if @comment.save
-      redirect_back(fallback_location: boards_path)
+    comment = current_user.comments.build(comment_params)
+    if comment.save
+      #redirect_back(fallback_location: boards_path)
+      redirect_to board_path(comment.board), success: t('defaults.message.created', item: Comment.model_name.human)
     else
-      redirect_back(fallback_location: boards_path)
+      #redirect_back(fallback_location: boards_path)
+      redirect_to board_path(comment.board), danger: t('defaults.message.not_created', item: Comment.model_name.human)
     end
   end
+
 
   private
 
