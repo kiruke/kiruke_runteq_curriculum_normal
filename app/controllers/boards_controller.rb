fd8b645a5d0cf16109a57b7aa0a3f429c58a9ace
kiruke <kiruke2525@gmail.com>
2022-05-03 14:15:31 +0900

    掲示板編集削除機能アクション追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :set_params, only: [:edit, :update, :destroy]
 
   def index
     #includesでN+1問題を解決
@@ -13,7 +14,7 @@ class BoardsController < ApplicationController
     @board = current_user.boards.build(board_params)
     if @board.save
       flash[:success] = t('.success')
-      redirect_back_or_to boards_url
+      redirect_to boards_url
     else
       flash.now[:danger] = t('.fail')
       render :new
@@ -26,10 +27,34 @@ class BoardsController < ApplicationController
     @comments = @board.comments.includes(:user).order(created_at: :desc)
   end
 
+  def edit
+    #@board = current_user.boards.find(params[:id])
+  end
+
+  def update
+    #@board = current_user.boards.find(params[:id])
+    if @board.update(board_params)
+      redirect_to @board, success: t('defaults.message.updated', item: Board.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: Board.model_name.human)
+      render :edit
+    end
+  end
+
+  def destroy
+    #@board = current_user.boards.find(params[:id])
+    @board.destroy!
+    redirect_to boards_url, success: t('defaults.message.deleted', item: Board.model_name.human)
+  end
+
 
   private
 
   def board_params
     params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
+  end
+  
+  def set_params
+    @board = current_user.boards.find(params[:id])
   end
 end
