8998ce33d87123aa9dc8813dc90096ff37d4fb9f
kiruke <kiruke2525@gmail.com>
2022-05-10 21:23:36 +0900

    bookmarkしているかのメソッドをboardモデルに追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,4 +5,9 @@ class Board < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65535 }
+  
+  def bookmarked_by?(user)
+    bookmarks.where(user_id: user).exists?
+  end
+
 end
