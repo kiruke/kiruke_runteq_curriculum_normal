5c6844b7e2b5b52c5cdb5f5e1753c2047626a809
kiruke <kiruke2525@gmail.com>
2022-05-14 16:36:17 +0900

    ブックマークボタンのajax処理

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,16 +3,20 @@ class BookmarksController < ApplicationController
   def create
     #board_idとuser_idの組み合わせをbookmarkテーブルに保存する
     bookmark = current_user.bookmarks.build(board_id: params[:board_id])
+    @board = Board.find(bookmark.board_id)
     bookmark.save
-    redirect_to boards_path, success: t('.success')
-    #redirect_back fallback_location: root_path, success: t('.success')
+    #redirect_to boards_path, success: t('.success')
   end
 
   def destroy
     bookmark = current_user.bookmarks.find(params[:id])
+    @board = Board.find(bookmark.board_id)
     bookmark.destroy!
-    redirect_to request.referer, success: t('.success')
-    #redirect_back fallback_location: root_path, success: t('.success')
+    #-------解答例-----------
+    #@board = Board.find(params[:board_id])
+    #current_user.bookmark(board)
+    #-----------------------
+    #redirect_to request.referer, success: t('.success')
   end
   
 end
