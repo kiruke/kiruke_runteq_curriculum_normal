1a345fa982808ae1aaa7f12803c10f816d7a25d1
kiruke <kiruke2525@gmail.com>
2022-06-01 14:47:00 +0900

    letter_openerのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'static_pages#top'
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :bookmarks, only: [:create, :destroy]
   resource :profiles, only: [:show, :edit, :update]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   resources :boards do
     get 'bookmarks', 'serach' , on: :collection
     resources :comments, shallow: true
