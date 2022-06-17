43ae58fd5cc85d57c453c5d7759b9e58da5c2f01
kiruke <kiruke2525@gmail.com>
2022-06-17 17:02:27 +0900

    管理画面viewの作成

--- app/controllers/admin/boards_controller.rb
+++ app/controllers/admin/boards_controller.rb
@@ -1,4 +1,6 @@
 class Admin::BoardsController < Admin::BaseController
+  before_action :set_board, only: [:show, :edit, :update, :destroy]
+
   def index
     @q = Board.ransack(params[:q])
     @boards = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
@@ -13,11 +15,17 @@ class Admin::BoardsController < Admin::BaseController
   end
 
   def update
-
+    if @board.update(board_params)
+      redirect_to admin_boards_path, success: t('defaults.message.updated', item: Board.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: Board.model_name.human)
+      render :edit
+    end
   end
 
   def destroy
-
+    @board.destroy!
+    redirect_to admin_boards_url, success: t('defaults.message.deleted', item: Board.model_name.human)
   end
 
   private
@@ -27,6 +35,6 @@ class Admin::BoardsController < Admin::BaseController
   end
   
   def set_board
-    @board = current_user.boards.find(params[:id])
+    @board = Board.find(params[:id])
   end
 end
